e6cf9c9e44f61d6f5fedc7e911ee2b753423f210
Craig Dietrich <craigdietrich@gmail.com>
2018-07-30 16:18:49 -0700

    Style updates

--- CommonPlace/MainPage.xaml.cs
+++ CommonPlace/MainPage.xaml.cs
@@ -19,13 +19,8 @@ using Windows.Data.Json;
 using Windows.ApplicationModel.Email;
 using Windows.ApplicationModel.Background;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace CommonPlace
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
