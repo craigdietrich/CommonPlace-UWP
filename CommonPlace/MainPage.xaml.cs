cde64920fcf0438d88c7677b8386472d93e73532
Craig Dietrich <craigdietrich@gmail.com>
2018-08-30 15:57:01 -0700

    Send email

--- CommonPlace/MainPage.xaml.cs
+++ CommonPlace/MainPage.xaml.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Windows.UI.ViewManagement;
 using Windows.ApplicationModel.Core;
 using Windows.UI;
+using Windows.UI.Popups;
+using System.Xml.Linq;
+using Windows.ApplicationModel;
 using Windows.Data.Json;
+using LightBuzz.SMTP;
 using Windows.ApplicationModel.Email;
-using Windows.ApplicationModel.Background;
 
 namespace CommonPlace
 {
@@ -43,6 +36,10 @@ namespace CommonPlace
             string src = "ms-appx-web:///Assets/index.html";
             //await WebView.ClearTemporaryWebDataAsync();
             this.MyWebView.Navigate(new Uri(src));
+
+            // Alert for testing
+            //MessageDialog showDialog = new MessageDialog("smtpPassword: " + smtpPassword);
+            //var result = await showDialog.ShowAsync();
         }
 
         private void MyWebView_ContentLoading(WebView sender, WebViewContentLoadingEventArgs args)
@@ -52,12 +49,32 @@ namespace CommonPlace
 
         private async void MainWebView_ScriptNotify(object sender, NotifyEventArgs e)
         {
-           // JObject json = JObject.Parse(e.Value);
-            string method = "email";
+            var json = JsonValue.Parse(e.Value).GetObject();
+            string method = json.GetObject().GetNamedString("method");
             switch (method)
             {
                 case "email":
-                    var emailMessage = new Windows.ApplicationModel.Email.EmailMessage();
+                    // Load Email SMTP settings
+                    string XMLFilePath = Path.Combine(Package.Current.InstalledLocation.Path, "EmailConfig.xml");
+                    XDocument loadedData = XDocument.Load(XMLFilePath);
+                    XElement generalElement = loadedData.Element("emailSettings");
+                    string smtpHost = (string)generalElement.Element("smtpHost");
+                    string smtpUsername = (string)generalElement.Element("smtpUsername");
+                    string smtpPassword = (string)generalElement.Element("smtpPassword");
+                    string smtpSecure = (string)generalElement.Element("smtpSecure");
+                    string smtpPort = (string)generalElement.Element("smtpPort");
+                    // Load values from interface
+                    string address = json.GetObject().GetNamedString("address");
+                    string title = json.GetObject().GetNamedString("title");
+                    string url = json.GetObject().GetNamedString("url");
+                    // Create email
+                    SmtpClient client = new SmtpClient(smtpHost, int.Parse(smtpPort), true, smtpUsername, smtpPassword);
+                    EmailMessage emailMessage = new EmailMessage();
+                    emailMessage.To.Add(new EmailRecipient(address));
+                    //emailMessage.Bcc.Add(new EmailRecipient("someone3@anotherdomain.com"));
+                    emailMessage.Subject = "[OXY CommonPlace] "+title;
+                    emailMessage.Body = "<b>" + title + "</b>\r\n\r\n" + url;
+                    await client.SendMailAsync(emailMessage);
                     break;
             }
         }
